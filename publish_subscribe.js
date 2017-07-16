9d5a45a7469fb8aa6775d3b9b127db1aea86a51b
LiwenChii <328957392@qq.com>
2017-07-16 21:47:27 +0800

    add remove function

--- publish_subscribe.js
+++ publish_subscribe.js
@@ -17,6 +17,23 @@ var event = {
     for(var i = 0, fn; fn = fns[i++];){
       fn.apply(this, arguments);
     }
+  },
+  remove: function(key, fn){
+    var fns = this.clientList[key];
+
+    if(!fns){
+      return false;
+    }
+    if(!fn){
+      fns && (fns.length = 0);
+    } else {
+      for(var l = fns.length - 1; l >= 0; l--){
+        var _fn = fns[l];
+        if(_fn === fn){
+          fns.splice(l, 1);
+        }
+      }
+    }
   }
 };
 
@@ -30,7 +47,11 @@ var installEvent = function(obj){
 var salesOffices = {};
 installEvent(salesOffices);
 
-salesOffices.listen('squareMeter88', function(price){
+salesOffices.listen('squareMeter88', fn1 = function(price){
+  console.log('价格= ' + price);
+})
+
+salesOffices.listen('squareMeter88', fn2 = function(price){
   console.log('价格= ' + price);
 })
 
@@ -40,3 +61,6 @@ salesOffices.listen('squareMeter100', function(price){
 
 salesOffices.trigger('squareMeter88', 2000000);
 salesOffices.trigger('squareMeter100', 3000000);
+
+salesOffices.remove('squareMeter88', fn1);
+salesOffices.trigger('squareMeter88', 2000000);
